99f53bd40e47d7ac75480f11174900663f76fc37
margo250494@gmail.com <Margo250494@github.com>
2024-12-10 19:44:09 +0100

    alert

--- src/components/SubmitForm/useFormData.js
+++ src/components/SubmitForm/useFormData.js
@@ -20,8 +20,26 @@ export const useFormData = () => {
 
     const submit = (event) =>{
         event.preventDefault();
-        setSubmittedData(formData);
-        setFormData({ userName: "", fullName: "", age: "" });
+        try {
+            if(!formData.userName ||!formData.fullName ||formData.age === ""){
+                throw new Error("Please fill in all the information");
+            }
+            if (!formData.userName) {
+                throw new Error("User Name is required");
+            }
+            if (!formData.fullName) {
+                throw new Error("Full Name is required");
+            }
+            if (formData.age === "") {
+                throw new Error("Age is required");
+            }
+            setSubmittedData(formData);
+            setFormData({ userName: "", fullName: "", age: "" });
+    
+        } catch (error) {
+            alert(error.message);
+        }
+
     }
 
     const handleAgeChange = (e) => {
